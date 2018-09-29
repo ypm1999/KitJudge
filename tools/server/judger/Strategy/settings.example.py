5ebc74c39da8b375b1e1047884cd812c5252196b
Hzfengsy <Hzfengsy@vip.qq.com>
2018-09-29 19:42:29 +0800

    Release After PPCA 2018

---
+++ tools/server/judger/Strategy/settings.example.py
@@ -0,0 +1,15 @@
+kitReportMQHost = '***'
+kitReportMQPort = 5672
+kitReportMQUsername = '***'
+kitReportMQPassword = '***'
+kitReportMQQueueName = '***'
+kitReportMQHeartBeat = 20
+
+kitGitHost = '***'
+kitGitUser = '***'
+
+kitDBPort = 3306
+kitDBHost = "***"
+kitDBName = "***"
+kitDBUsername = "***"
+kitDBPassword = "***"
